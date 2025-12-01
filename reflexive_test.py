cc7dfb41b85b92ba6f971818acd729567698d596
kbb306 <kylerbbelshaw@gmail.com>
2025-12-01 00:05:06 -0700

    Fixed bad call to set_row()

--- reflexive_test.py
+++ reflexive_test.py
@@ -8,12 +8,13 @@ Watchowski = Matrix(row,col)
 for i in range(row):
     inrow = []
     for j in range(col):
-        inrow.append(int(input("Enter next value: ")))
-    Watchowski.set_row(row,inrow)
+        inrow.append(int(input(f"Enter next value in row {i+1}: ")))
+    Watchowski.set_row((i+1),inrow)
 
 check = []
 
 Reflexive = True
+Watchowski.print_matrix()
 for each in Watchowski.Neo:
     get = Watchowski.get_val(Watchowski.Neo.index(each),Watchowski.Neo.index(each))
     if get < 1:

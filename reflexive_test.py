84844790171ca0031020d99efe3943d708b0c58d
kbb306 <kylerbbelshaw@gmail.com>
2025-11-30 23:21:44 -0700

    Good grief. Why can't I remember how my own class works?

--- reflexive_test.py
+++ reflexive_test.py
@@ -8,7 +8,7 @@ Watchowski = Matrix(row,col)
 for each in Watchowski.Neo:
     for val in each:
         inval = input(f"Enter row {Watchowski.Neo.index(each) + 1} column {each.index(val) + 1} of matrix ")
-        Watchowski.set_val(inval)
+        Watchowski.set_val((Watchowski.Neo.index(each) + 1),(each.index(val) + 1),inval)
 
 check = []
 

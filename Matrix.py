136c47dbcb12395e221ceb0ac8c8d2325cb2ff39
kbb306 <kylerbbelshaw@gmail.com>
2025-11-30 23:16:20 -0700

    Added integer guards for all row and column inputs

--- Matrix.py
+++ Matrix.py
@@ -1,18 +1,18 @@
 
 class Matrix():
     def __init__(self, row, col):
-        row = abs(row)
-        col = abs(col)
+        row = abs(int(row))
+        col = abs(int(col))
         self.Neo = []
         for i in range(row - 1):
             listinator = [j for j in range(col - 1)]
             self.Neo.append(listinator)
 
     def get_row(self,row):
-        row = abs(row)
+        row = abs(int(row))
         return self.Neo[row - 1]
     def set_row(self,row,Smith):
-        row = abs(row)
+        row = abs(int(row))
         if not all(isinstance(item, int) for item in Smith):
             raise ValueError("Input has non-integers")
         if len(Smith) == len(self.Neo[row - 1]):
@@ -25,13 +25,13 @@ class Matrix():
             raise ValueError("Input has too many elements")
         
     def get_col(self,col):
-        col = abs(col)
+        col = abs(int(col))
         Morpheus = []
         for each in self.Neo:
             Morpheus.append(each[col-1])
         return Morpheus
     def set_col(self,col,Morpheus):
-        col = abs(col)
+        col = abs(int(col))
         if not all(isinstance(item, int) for item in Morpheus):
             raise ValueError("Input has non-integers")
         if len(Morpheus) > self.Neo[0]:
@@ -42,15 +42,15 @@ class Matrix():
             each[col] = Morpheus[self.Neo.index(each)]
 
     def get_val(self,row,col):
-        row = abs(row)
-        col = abs(col)
+        row = abs(int(row))
+        col = abs(int(col))
         if (row > len(self.Neo) or col > len(self.Neo)):
             raise ValueError(f"Index {row},{col} out of range.")
         else:
             return self.Neo[row -1][col -1]
     def set_val(self,row,col,Trinity):
-        row = abs(row)
-        col = abs(col)
+        row = abs(int(row))
+        col = abs(int(col))
         if not isinstance(Trinity,int):
             raise ValueError(f"{Trinity} is not an integer")
         if (row > len(self.Neo) or col > len(self.Neo)):

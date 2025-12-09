9828116e76548d1e5f3f45089d1d0f99a4b08096
kbb306 <kylerbbelshaw@gmail.com>
2025-12-08 18:12:07 -0700

    Finished prototype symmetry test

--- symmetry_test.py
+++ symmetry_test.py
@@ -3,4 +3,19 @@ rows = int(input("Enter rows of matrix: "))
 cols = int(input("Enter columns of matrix: "))
 
 Reeves = Matrix(rows,cols)
-Seveeer = Matrix(cols,rows)
+Seveer = Matrix(cols,rows)
+
+for i in range(rows):
+    inrow = []
+    for j in range(cols):
+        inrow.append(int(input(f"Enter next value in row {i+1}: ")))
+    Reeves.set_row((i+1),inrow)
+    try:
+        Seveer.set_col(i+1,inrow)
+    except ValueError:
+        print("Your matrix is not symmetrical")
+        break
+if Reeves.Neo == Seveer.Neo:
+    print("Your matrix is symmetrical!")
+else:
+    print("Your matrix is not symmetrical!")
